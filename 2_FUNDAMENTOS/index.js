239ab49f5933d34903929e1eb4cc0a51160526ad
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-04-24 10:34:55 -0300

    Iniciando o curso de TypeScript

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,8 @@
+//1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.1452;
+console.log(y);
+console.log(typeof y);
---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,11 @@
+//1 - numbers
+let x:number = 10;
+
+console.log(x);
+
+x = 16;
+console.log(typeof x);
+
+let y:number = 15.1452
+console.log(y);
+console.log(typeof y);
