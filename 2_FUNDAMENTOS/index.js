d358746ec8a4e848f3f93938c129110dea0d5a31
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-04-24 15:29:00 -0300

    fundamentos de string e boolean com typescript

--- 2_FUNDAMENTOS/index.js
+++ 2_FUNDAMENTOS/index.js
@@ -6,3 +6,15 @@ console.log(typeof x);
 var y = 15.1452;
 console.log(y);
 console.log(typeof y);
+//2 - string
+var firstName = "Matheus";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Battisti";
+fullName = "".concat(firstName, " ").concat(lastName);
+console.log(fullName);
+console.log(typeof fullName);
+//3 - boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -9,3 +9,22 @@ console.log(typeof x);
 let y:number = 15.1452
 console.log(y);
 console.log(typeof y);
+
+//2 - string
+const firstName:string = "Matheus";
+console.log(firstName.toUpperCase());
+
+let fullName:string;
+
+const lastName:string = "Battisti";
+
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(typeof fullName);
+
+//3 - boolean
+let a:boolean = false;
+console.log(a);
+console.log(typeof a);
+
+
